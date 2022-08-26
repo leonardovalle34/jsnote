0ab62175ea643c891cdbe83d7f1f58fb7104e43d
Leonardo do Valle <107518514+leonardovalle34@users.noreply.github.com>
2022-08-26 10:16:25 -0300

    react head

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Fragment } from 'react';
 import "./App.scss";
 import RoutesIn from './routes';
+import {Helmet} from "react-helmet"
 
 
 
 
 const App = () => (
   <Fragment>
+      <Helmet>
+          <meta charSet="utf-8" />
+          <title>Your notes on the clouds!</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+          <meta name='description' content='js notes'/>
+      </Helmet>
       <RoutesIn/>
   </Fragment>
 )
